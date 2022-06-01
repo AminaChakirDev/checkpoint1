b96a081f33bdbbb016b30b7b7c602d6363de1d8b
NDook <thomasmludek@gmail.com>
2022-06-01 15:32:32 +0200

    step 3 - some JS - html excercice

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -11,13 +11,15 @@ imgBan.addEventListener("click", function(){
 //Action bouton Modify text and color
 //je selectionne le buton
 const buttonTextColr = document.querySelector(".textColorButton");
-//je recupère le nom à modifier
+//je recupère les element à modifier
 const nameToModify = document.querySelector("#name");
+const backGroundColor = document.querySelector(".pink-bg");
 //je crée un addeventlistenner lorsque l'on clique sur le bouton et je modifie les propriétés
 buttonTextColr.addEventListener("click", function() {
-    const backgraoundColor = window.prompt("Enter a color: ")
+    const modifyBackgroundColor = window.prompt("Enter a color: ")
     const user = window.prompt("Enter your name: " );
-
+    backGroundColor.style.backgroundColor = modifyBackgroundColor;
+    buttonTextColr.style.backgroundColor = modifyBackgroundColor;
     nameToModify.innerHTML = user;
     nameToModify.style.color = "white";
 });
