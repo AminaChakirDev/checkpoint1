868f13c6eb29ea32cf6e86257560e389fb4a6935
NDook <thomasmludek@gmail.com>
2022-06-01 15:50:00 +0200

    bonus1 - some JS - html excercice

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -15,18 +15,27 @@ const buttonTextColr = document.querySelector(".textColorButton");
 const nameToModify = document.querySelector("#name");
 const backGroundColor = document.querySelectorAll(".pink-bg");
 const allPinkElement = document.querySelectorAll(".pink-text");
+const allLink = document.querySelectorAll(".link");
 //je crée un addeventlistenner lorsque l'on clique sur le bouton et je modifie les propriétés
 buttonTextColr.addEventListener("click", function() {
-    const modifyBackgroundColor = window.prompt("Enter a color: ")
+    const modifyColor = window.prompt("Enter a color: ")
     const user = window.prompt("Enter your name: " );
     backGroundColor.forEach(element => {
-        element.style.backgroundColor = modifyBackgroundColor;
+        element.style.backgroundColor = modifyColor;
     });
     allPinkElement.forEach(element =>{
-        element.style.color = modifyBackgroundColor;
+        element.style.color = modifyColor;
     });
+    allLink.forEach(element => {
+        element.style.color = modifyColor;
+    })
     //backGroundColor.style.backgroundColor = modifyBackgroundColor; remplacé par les forEach sur tous les element du tableau
-    buttonTextColr.style.backgroundColor = modifyBackgroundColor;
+    buttonTextColr.style.backgroundColor = modifyColor;
     nameToModify.innerHTML = user;
     nameToModify.style.color = "white";
 });
+
+//Bonus1 : changer les couleur des liens
+//je selectionne tous les liens et les stockent dans une variable
+
+//je crée un addeventlistener 
