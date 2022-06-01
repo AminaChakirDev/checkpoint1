96354aed8298e7b5743366441a54ada7361c52ab
NDook <thomasmludek@gmail.com>
2022-06-01 15:41:32 +0200

    step 4 - some JS - html excercice

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -13,12 +13,19 @@ imgBan.addEventListener("click", function(){
 const buttonTextColr = document.querySelector(".textColorButton");
 //je recupère les element à modifier
 const nameToModify = document.querySelector("#name");
-const backGroundColor = document.querySelector(".pink-bg");
+const backGroundColor = document.querySelectorAll(".pink-bg");
+const allPinkElement = document.querySelectorAll(".pink-text");
 //je crée un addeventlistenner lorsque l'on clique sur le bouton et je modifie les propriétés
 buttonTextColr.addEventListener("click", function() {
     const modifyBackgroundColor = window.prompt("Enter a color: ")
     const user = window.prompt("Enter your name: " );
-    backGroundColor.style.backgroundColor = modifyBackgroundColor;
+    backGroundColor.forEach(element => {
+        element.style.backgroundColor = modifyBackgroundColor;
+    });
+    allPinkElement.forEach(element =>{
+        element.style.color = modifyBackgroundColor;
+    });
+    //backGroundColor.style.backgroundColor = modifyBackgroundColor; remplacé par les forEach sur tous les element du tableau
     buttonTextColr.style.backgroundColor = modifyBackgroundColor;
     nameToModify.innerHTML = user;
     nameToModify.style.color = "white";
