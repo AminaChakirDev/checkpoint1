da4c8d5561a8590593e15d4d3e9860bbb2886353
NDook <thomasmludek@gmail.com>
2022-06-01 16:33:32 +0200

    bonus2 - some JS - html excercice

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -9,7 +9,8 @@ imgBan.addEventListener("click", function(){
 });
 
 //Action bouton Modify text and color
-//je selectionne le buton
+//je selectionne les boutons
+const mainButton = document.querySelectorAll(".mainButton");
 const buttonTextColr = document.querySelector(".textColorButton");
 //je recupère les element à modifier
 const nameToModify = document.querySelector("#name");
@@ -20,22 +21,37 @@ const allLink = document.querySelectorAll(".link");
 buttonTextColr.addEventListener("click", function() {
     const modifyColor = window.prompt("Enter a color: ")
     const user = window.prompt("Enter your name: " );
+    //changer la couleur de tous les background
     backGroundColor.forEach(element => {
         element.style.backgroundColor = modifyColor;
     });
+    //changer la couleur des textes pink
     allPinkElement.forEach(element =>{
         element.style.color = modifyColor;
     });
+    //changer la couleur des liens
     allLink.forEach(element => {
         element.style.color = modifyColor;
     })
-    //backGroundColor.style.backgroundColor = modifyBackgroundColor; remplacé par les forEach sur tous les element du tableau
-    buttonTextColr.style.backgroundColor = modifyColor;
+    //changer la  couleur des boutons
+    mainButton.forEach(element => {
+        element.style.backgroundColor = modifyColor;
+    })
+    //modifier le texte du prénom utilisateur et sa couleur
     nameToModify.innerHTML = user;
     nameToModify.style.color = "white";
 });
 
-//Bonus1 : changer les couleur des liens
-//je selectionne tous les liens et les stockent dans une variable
-
-//je crée un addeventlistener 
+//Bonus1 : modifier les textes " you can modify"
+//je recupère le bouton dans une variable
+const devButton = document.querySelector(".blockButton");
+//je selectionne les li
+const liModify = document.querySelectorAll(".liModify");
+//je créer un tableau avec les données de sortie
+let arrayDev = ["VS Code", "GitHub", "Terminal"];
+//je crée un addeventlistener sur devButton pour changer les li
+devButton.addEventListener("click", function(event) {
+    for (let i=0; i < liModify.length; i++) {
+        liModify[i].innerHTML = arrayDev[i];
+    }
+})
