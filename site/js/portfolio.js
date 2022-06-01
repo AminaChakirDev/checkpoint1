78da8aa262070def096a8adc855fd9ec8129b626
NDook <thomasmludek@gmail.com>
2022-06-01 15:20:18 +0200

    step 2 - some JS - html excercice

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,8 +1,21 @@
 console.log("connecté");
 
+//modifcation image de bannière
 //je selectionne l'image et stocke dans une variable
 const imgBan = document.querySelector(".imgBan");
 //je crrer un addeventlistener pour modifier la source lors du clique sur l'image
 imgBan.addEventListener("click", function(){
     imgBan.src ="image/avatar.svg";
+});
+
+//Action bouton Modify text and color
+//je selectionne le buton
+const buttonTextColr = document.querySelector(".textColorButton");
+//je recupère le nom à modifier
+const nameToModify = document.querySelector("#name");
+//je crée un addeventlistenner lorsque l'on clique suyr le bouton et je modifie les propriétés
+buttonTextColr.addEventListener("click", function() {
+    const user = window.prompt("Entyer your name: " );
+    nameToModify.innerHTML = user;
+    nameToModify.style.color = "white";
 });
