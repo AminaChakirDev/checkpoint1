d2ffe5aa63ecdb9239be83fb05bc68067640110a
NDook <thomasmludek@gmail.com>
2022-06-01 15:05:02 +0200

    step 1 - some JS - html excercice

---
+++ site/js/portfolio.js
@@ -0,0 +1,8 @@
+console.log("connecté");
+
+//je selectionne l'image et stocke dans une variable
+const imgBan = document.querySelector(".imgBan");
+//je crrer un addeventlistener pour modifier la source lors du clique sur l'image
+imgBan.addEventListener("click", function(){
+    imgBan.src ="image/avatar.svg";
+});
